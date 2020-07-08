8aa4246c418835e9b34d18c8be5e497ad25c91ae
Helton Isac <helton.isac@gmail.com>
2020-07-08 11:19:01 -0300

    Starting project2

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Helton Isac Torres Galindo on 08/07/20.
+//  Copyright © 2020 Helton Isac Torres Galindo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
