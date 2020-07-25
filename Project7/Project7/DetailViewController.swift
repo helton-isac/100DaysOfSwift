29f553a120e22d5e004b8b4c6be4e61ab85d4274
Helton Isac <helton.isac@gmail.com>
2020-07-25 17:05:15 -0300

    Adding a detail view controller

---
+++ Project7/Project7/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Project7
+//
+//  Created by Helton Isac Torres Galindo on 25/07/20.
+//  Copyright © 2020 Helton Isac Torres Galindo. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let detailItem = detailItem else {return}
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
+        
+        webView.loadHTMLString(html, baseURL: nil)
+        
+    }
+}
