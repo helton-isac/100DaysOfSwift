73ab64b7c1401dd8692d26974125c7dcd7ea3c9f
Helton Isac <helton.isac@gmail.com>
2020-07-29 21:50:49 -0300

    Creating filter to the list

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UITableViewController {
     
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +25,9 @@ class ViewController: UITableViewController {
             //urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
-        
+        title = "All"
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "About", style: .plain, target: self, action: #selector(showCredits))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterPetitionsAction))
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
@@ -49,17 +51,18 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            filteredPetitions = petitions
             tableView.reloadData()
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -67,7 +70,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
     
@@ -75,6 +78,43 @@ class ViewController: UITableViewController {
         let ac = UIAlertController(title: "Credits", message: "The data comes from the We The People API of the Whitehouse.", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
+    }
+    
+    @objc func filterPetitionsAction() {
+        let ac = UIAlertController(title: "Filter Petition", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let filterAction = UIAlertAction(title: "OK", style: .default) {
+            [weak self, weak ac] _ in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.filterPetitions(item)
+        }
+        
+        ac.addAction(filterAction)
+        
+        let cleanAction = UIAlertAction(title: "Clean", style: .default) {
+            [weak self, weak ac] _ in
+            self?.cleanFilter()
+        }
+        
+        ac.addAction(cleanAction)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func filterPetitions(_ textToFilter: String) {
+        filteredPetitions = petitions.filter({ (petition) -> Bool in
+            return petition.title.lowercased().contains(textToFilter.lowercased())
+                || petition.body.lowercased().contains(textToFilter.lowercased())
+        })
+        title = textToFilter
+        tableView.reloadData()
+    }
+    
+    func cleanFilter() {
+        filteredPetitions = petitions
+        title = "All"
+        tableView.reloadData()
     }
 }
 
