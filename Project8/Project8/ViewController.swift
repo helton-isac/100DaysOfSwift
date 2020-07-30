4117e3f95ae6775f2d3ae4f60570184e5876ed62
Helton Isac <helton.isac@gmail.com>
2020-07-30 19:25:24 -0300

    Day 36 - Creating the Project 8

---
+++ Project8/Project8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Helton Isac Torres Galindo on 30/07/20.
+//  Copyright © 2020 Helton Isac Torres Galindo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
