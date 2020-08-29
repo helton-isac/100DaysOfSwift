7cd4892526102bb9ce1778c801b011cdee727aff
Helton Isac <helton.isac@gmail.com>
2020-08-29 17:04:46 -0300

    Creating the Person View

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Helton Isac Torres Galindo on 29/08/20.
+//  Copyright © 2020 Helton Isac Torres Galindo. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var name: UILabel!
+    
+}
