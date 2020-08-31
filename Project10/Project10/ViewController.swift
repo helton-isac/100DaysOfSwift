ad283bbd29f8aca785e75cb37235ec424f994077
Helton Isac <helton.isac@gmail.com>
2020-08-31 20:41:39 -0300

    Changing the alert to delete and rename

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -71,12 +71,25 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
-        let ac = UIAlertController(title: "Rename Person", message: nil, preferredStyle: .alert)
-        ac.addTextField()
-        ac.addAction(UIAlertAction(title: "OK", style: .default){
-            [weak self, weak ac] _ in
-            guard let newName = ac?.textFields?[0].text else { return }
-            person.name = newName
+        let ac = UIAlertController(title: "Choose An Action", message: "Do you want to rename or delete this person?", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Rename", style: .default){
+            [weak self] _ in
+            self?.dismiss(animated: true)
+            
+            let ac = UIAlertController(title: "Rename Person", message: nil, preferredStyle: .alert)
+            ac.addTextField()
+            ac.addAction(UIAlertAction(title: "OK", style: .default){
+                [weak self, weak ac] _ in
+                guard let newName = ac?.textFields?[0].text else { return }
+                person.name = newName
+                self?.collectionView.reloadData()
+            })
+            ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            self?.present(ac, animated: true)
+        })
+        ac.addAction(UIAlertAction(title: "Delete", style: .default){
+            [weak self] _ in
+            self?.people.remove(at: indexPath.item)
             self?.collectionView.reloadData()
         })
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
