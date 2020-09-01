188b51a4b63279d91bf71b27a4ff41d5a78852e5
Helton Isac <helton.isac@gmail.com>
2020-08-31 21:57:54 -0300

    Improving Project 1 with collection view

---
+++ Project1_collection/Project1/StormCell.swift
@@ -0,0 +1,17 @@
+//
+//  StormCell.swift
+//  Project1
+//
+//  Created by Helton Isac Torres Galindo on 31/08/20.
+//  Copyright © 2020 Helton Isac Torres Galindo. All rights reserved.
+//
+
+import UIKit
+
+class StormCell: UICollectionViewCell {
+
+    @IBOutlet var stormImage: UIImageView!
+    
+    @IBOutlet var stormName: UILabel!
+    
+}
