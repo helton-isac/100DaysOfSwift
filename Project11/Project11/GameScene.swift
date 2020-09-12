d26fdbd240b2e262060a96a5aed1eeb2f0f09115
Helton Isac <helton.isac@gmail.com>
2020-09-12 19:30:29 -0300

    Adding images

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -11,5 +11,10 @@ import SpriteKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        addChild(background)
+        
     }
 }
