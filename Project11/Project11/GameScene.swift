9ca0495eb107c69312ca7991d6bb8027ee6c7996
Helton Isac <helton.isac@gmail.com>
2020-10-12 18:32:27 -0300

    Right now, users can tap anywhere to have a ball created there, which makes the game too easy. Try to force the Y value of new balls so they are near the top of the screen.

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -94,6 +94,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 ball.physicsBody?.restitution = 0.4
                 ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
                 ball.position = location
+                ball.position.y = 700
                 ball.name = "ball"
                 addChild(ball)
             }
