dcfdf23008e3d7b0c9356dc7324628c026370b3e
Helton Isac <helton.isac@gmail.com>
2020-12-07 21:41:55 -0300

    Creating Project12 a and b from project 10

---
+++ Project12a/Project12a/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project12a
+//
+//  Created by Helton Isac Torres Galindo on 30/08/20.
+//  Copyright © 2020 Helton Isac Torres Galindo. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
